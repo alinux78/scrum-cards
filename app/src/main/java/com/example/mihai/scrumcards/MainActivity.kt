b6cb4c5e35e924676e9ab9996055b21d2507c276
Mihai Casu <mcasu@ixiacom.com>
2018-01-11 15:21:32 +0200

    Use a GridView

--- app/src/main/java/com/example/mihai/scrumcards/MainActivity.kt
+++ app/src/main/java/com/example/mihai/scrumcards/MainActivity.kt
@@ -6,12 +6,18 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import android.widget.GridView
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val gridview = findViewById<View>(R.id.gridview) as GridView
+        gridview.adapter = ScrumCardsAdapter(this)
+
     }
 
     fun onCardClick(view: View) {
---
+++ app/src/main/java/com/example/mihai/scrumcards/ScrumCardsAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.mihai.scrumcards
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.Button
+
+/**
+ * Created by mihai on 1/11/18.
+ */
+class ScrumCardsAdapter(private val context: MainActivity) : BaseAdapter() {
+
+
+    companion object {
+        val CARDS = arrayOf("0", "1/2", "1", "2", "3", "5", "8", "13", "21", "34", "100", "?")
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var button : Button
+        if (convertView == null) {
+            button = Button(context)
+            button.height = 420
+            button.setOnClickListener(context::onCardClick)
+        } else {
+            button = convertView as Button
+        }
+        button.text = CARDS[position]
+        return button;
+    }
+
+    override fun getItem(position: Int): Any {
+        return 0
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getCount(): Int {
+        return CARDS.size
+    }
+}
